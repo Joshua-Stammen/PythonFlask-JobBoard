59418f73d51201008f5d4ccbd268c1ebbfda2448
Joshua Stammen <joshuastammen.mail.weber.edu>
2019-03-13 12:07:48 -0600

    Dynamic Content

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,33 @@
+import sqlite3
 from flask import Flask, render_template
 
+PATH = 'db/jobs.sqlite'
+
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g,'connection', None)
+    if connection == None:
+        connection = g.connetion = sqlite3.connect(PATH)
+    connection.row.factory = sqlite3.row
+    return connection
+
+def execute_sql(sql,values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql,values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
